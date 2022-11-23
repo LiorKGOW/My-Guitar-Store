7b0fb3cbc99c6aa870f7d777a0aa3b8faa572a0b
Lior Keren <lkeren@redhat.com>
2022-11-23 18:42:32 +0200

    removing mock data from __mocks__/axios.js file

--- app/__mocks__/axios.js
+++ app/__mocks__/axios.js
@@ -1,18 +1,6 @@
 export default {
-  get: jest.fn().mockResolvedValue({
-    data: [
-      {
-        name: 'guitar1_test',
-        url: 'https://rukminim1.flixcart.com/image/416/416/acoustic-guitar/x/8/w/topaz-blue-signature-original-imaefec7uhypjdr9.jpeg?q=70',
-        price: '100',
-        description: 'some description in guitar1_test'
-      },
-      {
-        name: 'guitar2_test',
-        url: 'https://shop.brianmayguitars.co.uk/user/special/content/Antique%20Cherry%20a.jpg',
-        price: '200',
-        description: 'some description in guitar2_test'
-      }
-    ]
-  })
+  get: jest.fn(),
+  post: jest.fn(),
+  put: jest.fn(),
+  delete: jest.fn()
 };

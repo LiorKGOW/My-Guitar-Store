56f8acdb75d2e7576242102c6b277315d21e2368
Lior Keren <lkeren@redhat.com>
2022-12-13 18:15:17 +0200

    Generating GuitarsController Using Rails Generate Controller
    Replacing the existing controller created with the mock data

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
+  get 'guitars/guitar_gallery'
+  get 'guitars/welcome_page'
+
+  # route to get the mock data:
   get '/guitars', to: 'guitars#index'
+
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

523ce9b5dea83942ca4f20fb3f2b8d5de580c8da
Lior Keren <lkeren@redhat.com>
2023-01-04 16:50:05 +0200

    Generating Guitar Controller & Model, CRUD Actions
    Generating Guitar Controller & Model with empty CRUD actions
    Using rails generate controller & model
    Making the application present data in the DB instead of the mock data
    Adding seed to populate the db with a few guitars

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'guitars/index'
+  get 'guitars/show'
+  get 'guitars/new'
+  get 'guitars/edit'
+  get 'guitars/delete'
   root 'home#index'
   get '/guitars', to: 'guitars#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Guitar.destroy_all
+
+Guitar.create!([
+                 {
+                   name: 'guitar1',
+                   url: 'https://rukminim1.flixcart.com/image/416/416/acoustic-guitar/x/8/w/topaz-blue-signature-original-imaefec7uhypjdr9.jpeg?q=70',
+                   price: '100',
+                   description: 'blablabla 1'
+                 },
+                 {
+                   name: 'guitar2',
+                   url: 'https://shop.brianmayguitars.co.uk/user/special/content/Antique%20Cherry%20a.jpg',
+                   price: '200',
+                   description: 'blablabla 2'
+                 }
+               ])
+
+Rails.logger.debug { "Created #{Guitar.count} Guitars." }

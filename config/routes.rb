58b084a046426857a25669baf6af54e0260f606b
Lior Keren <lkeren@redhat.com>
2023-01-05 16:43:44 +0200

    Adding Resourceful Routes to Guitars Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/guitars', to: 'guitars#index'
+
+  resources :guitars
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

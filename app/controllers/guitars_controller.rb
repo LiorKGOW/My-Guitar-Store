806cbd13d4fb41978e6a8d408b7a9a3a39f939bc
Lior Keren <lkeren@redhat.com>
2023-01-05 16:43:51 +0200

    Presenting Data From the DB Instead of Mock Data
    + Adding some seed to populate the DB with a few guitars

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -1,29 +1,8 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/MethodLength
-
 class GuitarsController < ApplicationController
   def index
-    render json: [
-      {
-        name: 'guitar1',
-        url: 'https://rukminim1.flixcart.com/image/416/416/acoustic-guitar/x/8/w/topaz-blue-signature-original-imaefec7uhypjdr9.jpeg?q=70',
-        price: '100',
-        description: 'blablabla 1'
-      },
-      {
-        name: 'guitar2',
-        url: 'https://shop.brianmayguitars.co.uk/user/special/content/Antique%20Cherry%20a.jpg',
-        price: '200',
-        description: 'blablabla 2'
-      },
-      {
-        name: 'guitar3',
-        url: 'https://cdn.mos.cms.futurecdn.net/Yh6r74b8CAj2jbdf2FAhq4-970-80.jpg.webp',
-        price: '300',
-        description: 'blablabla 3'
-      }
-    ]
+    render json: Guitar.all
   end
 
   def show; end
@@ -40,5 +19,3 @@ class GuitarsController < ApplicationController
 
   def destroy; end
 end
-
-# rubocop:enable Metrics/MethodLength

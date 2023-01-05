adf958962da59e0759e65ab2c11e56aa741bb8d7
Lior Keren <lkeren@redhat.com>
2023-01-05 11:34:02 +0200

    Adding New & Create to Guitars Conrtoller

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -7,15 +7,36 @@ class GuitarsController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @guitar = Guitar.new
+    # Present a form to create a new Guitar (render the new template)
+  end
 
   def edit; end
 
-  def create; end
+  def create
+    # Instantiate a new Guitar using form parameters
+    @guitar = Guitar.new(guitar_params)
+    # Try to save the guitar to the DB
+    if @guitar.save
+      # If save succeeds, redirect to the index action
+      redirect_to(guitars_path)
+    else
+      # If save fails, redisplay the form so the user can fix problems
+      render(:new)
+    end
+  end
 
   def update; end
 
   def delete; end
 
   def destroy; end
+
+  private
+
+  def guitar_params
+    params.require(:guitar).permit(:name, :url, :price, :description)
+  end
+
 end
